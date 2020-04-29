645ca33d2d5d98dd979ab7d503baa1cb9c007030
Agustin Velasco Torres <aguslb@icloud.com>
2020-04-28 23:28:14 -0500

    Refreshing proyect

---
+++ src/main/java/com/udemyAgus/checkpoint/Checkpoint1Application.java
@@ -0,0 +1,13 @@
+package com.udemyAgus.checkpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemyAgus/checkpoint/Checkpoint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemyAgus.checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Checkpoint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
